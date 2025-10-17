de90271e10071015c1ba98911343a59044c9a6b2
SANJAY-1508 <psanjaykumar377@gmail.com>
2025-10-17 15:26:33 +0530

    `Refactor AppBar component: update CSS class names, add animation, and modify layout`

--- src/pages/components/AppBar.js
+++ src/pages/components/AppBar.js
@@ -1,32 +1,40 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 const AppBar = () => {
   return (
-    <>
-      <>
-        <div className="mark-bg">
-          <div>
-            <Container fluid>
-              <Row className="py-2">
-                <Col lg="4" xs="4" sm="4" className="align-self-center  ">
-                  <div className="d-flex align-items-center">
-                    <div>
-                      <img
-                        src={require("../../assets/logo.jpeg")}
-                        className="img-fluid gpay"
-                        alt="gpay"
-                      />
-                    </div>
-                  </div>
-                </Col>
-                <Col lg="8" className="align-self-center "></Col>
-              </Row>
-            </Container>
-          </div>
-        </div>
-      </>
-    </>
+    <div className="mark-bg py-2">
+      <Container fluid>
+        <Row className="align-items-center">
+          {/* Left: Logo */}
+          <Col lg={4} xs={4} sm={4} className="d-flex align-items-center">
+            <img
+              src={require("../../assets/logo.jpeg")}
+              className="img-fluid gpay"
+              alt="Logo"
+              style={{ maxHeight: "60px" }}
+            />
+          </Col>
+
+          {/* Center: Tamil Title */}
+          <Col lg={4} xs={4} sm={4} className="text-center">
+            <h4 className="appbar-title">
+              💰 கோபுரம் பைனான்ஸ் நகை அடகு கடை 💍
+            </h4>
+          </Col>
+
+          {/* Right: Animated Box */}
+          <Col lg={4} xs={4} sm={4} className="d-flex justify-content-end">
+            <Card className="blink-card text-center">
+              <Card.Body className="p-2">
+                <p className="mb-1 fw-bold">1 gram = 8000</p>
+                <p className="mb-0 fw-bold">1 pawn = 97.500</p>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
